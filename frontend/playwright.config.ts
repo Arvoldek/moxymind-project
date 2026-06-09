72309447c2ea0f501ce3615acc8be4b6880a4622
Arvoldek <arvoldek@duck.com>
2026-06-09 12:13:26 +0200

    feat: small updates

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,29 +1,28 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './src/tests',
-  fullyParallel: true,
+  testDir: "./src/tests",
   forbidOnly: !!process.env.CI,
   retries: 1,
-  reporter: [['html', { outputFolder: 'playwright-report' }], ['list']],
+  reporter: [["html", { outputFolder: "playwright-report" }], ["list"]],
   use: {
-    baseURL: 'https://www.saucedemo.com/',
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    baseURL: "https://www.saucedemo.com/",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
   ],
 });
